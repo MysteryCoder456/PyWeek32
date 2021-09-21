66d1fd775421545a7acda4909198a1cf831a20c4
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-09-21 18:48:37 +0400

    added parallax bg

--- Infinity Slasher/src/scripts/World.py
+++ Infinity Slasher/src/scripts/World.py
@@ -8,12 +8,29 @@ class World(Node2D):
 	platforms_velocity = Vector2(-250, 0)
 	platforms_acceleration = Vector2(-0.05, 0)
 	
+	bg_velocity = platforms_velocity / 15
+	bg_acceleration = platforms_acceleration / 15
+	
 	
 	def _ready(self):
+		self.background = self.get_node("Background")
 		self.platforms = self.get_node("Platforms")
 	
 	
 	def _physics_process(self, delta):
+		self.move_background(delta)
+		self.move_platforms(delta)
+		
+		
+	def move_background(self, delta):
+		self.bg_velocity += self.bg_acceleration
+		self.background.position += self.bg_velocity * delta
+		
+		if self.background.position.x <= -1000:
+			self.background.position += Vector2(1000, 0)
+		
+		
+	def move_platforms(self, delta):
 		self.platforms_velocity += self.platforms_acceleration
 		self.platforms.position += self.platforms_velocity * delta
 		
