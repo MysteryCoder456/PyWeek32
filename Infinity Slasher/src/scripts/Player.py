900d7a5b89d8340bd108e7836fc46e7e3b5627d1
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-09-22 23:03:00 +0400

    added attack animation

--- Infinity Slasher/src/scripts/Player.py
+++ Infinity Slasher/src/scripts/Player.py
@@ -4,10 +4,11 @@ from enum import Enum
 
 
 class State(Enum):
-	RUNNING	= 0
-	JUMPING	= 1
-	MID_AIR	= 2
-	FALLING	= 3
+	RUNNING		= 0
+	JUMPING		= 1
+	MID_AIR		= 2
+	FALLING		= 3
+	ATTACKING	= 4
 
 
 @exposed
@@ -16,7 +17,7 @@ class Player(KinematicBody2D):
 	GRAVITY = 30
 	
 	velocity = Vector2.ZERO
-	jump_force = Vector2(0, -500)
+	jump_force = Vector2(0, -550)
 	state = State.RUNNING
 	
 	
@@ -24,15 +25,20 @@ class Player(KinematicBody2D):
 		self.animated_sprite = self.get_node("AnimatedSprite")
 		self.magic_particles = self.get_node("MagicParticles")
 		
-		
-	def _physics_process(self, delta):
+	
+	def _process_input(self):
 		# Detect input here because _input function crashes game
+		
 		if Input.is_action_just_pressed("move_platform_up") and self.position.y > 256:
 			self.move_to_platform(-1)
 		elif Input.is_action_just_pressed("move_platform_down") and self.position.y < 512:
 			self.move_to_platform(1)
 		
-		# State machine
+		if Input.is_action_just_pressed("attack"):
+			self.attack()
+			
+		
+	def state_machine(self):
 		if self.state == State.RUNNING:
 			self.animated_sprite.play("run")
 			
@@ -55,9 +61,17 @@ class Player(KinematicBody2D):
 			
 			if self.is_on_floor():
 				self.state = State.RUNNING
+				
+		elif self.state == State.ATTACKING:
+			self.animated_sprite.play("attack")
+		
+		
+	def _physics_process(self, delta):
+		self._process_input()
+		self.state_machine()
 		
 		# Apply gravity
-		if not self.is_on_floor():
+		if not self.is_on_floor() and self.state != State.ATTACKING:
 			self.velocity.y += self.GRAVITY
 		
 		self.velocity = self.move_and_slide(self.velocity, Vector2.UP)
@@ -66,6 +80,12 @@ class Player(KinematicBody2D):
 	def jump(self):
 		self.velocity += self.jump_force
 		self.state = State.JUMPING
+		
+		
+	def attack(self):
+		self.velocity.y = 0
+		self.state = State.ATTACKING
+		# TODO: add code is destroy enemies
 		
 		
 	def move_to_platform(self, y_direction):
@@ -77,3 +97,6 @@ class Player(KinematicBody2D):
 		if self.state == State.JUMPING:
 			self.animated_sprite.play("mid air")  # to prevent animation glitches
 			self.state = State.MID_AIR
+			
+		elif self.state == State.ATTACKING:
+			self.state = State.FALLING
