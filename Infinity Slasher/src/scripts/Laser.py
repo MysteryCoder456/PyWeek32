33b01216fa19c384a803b62ba053682927276425
MysteryCoder456 <rehatbir.singh@gmail.com>
2021-09-23 10:11:19 +0400

    made the game become progressively harder

--- Infinity Slasher/src/scripts/Laser.py
+++ Infinity Slasher/src/scripts/Laser.py
@@ -1,11 +1,13 @@
 from godot import exposed, export
 from godot import *
+from random import randint
 
 
 @exposed
 class Laser(KinematicBody2D):
 	
 	velocity = export(Vector2)
+	damage = randint(5, 15)
 	
 	
 	def _physics_process(self, delta):
@@ -14,5 +16,8 @@ class Laser(KinematicBody2D):
 		
 		
 	def _on_CollisionDetector_body_entered(self, body):
+		if body.is_in_group("Player"):
+			self.get_tree().call_group("World", "damage_player", self.damage)
+			
 		self.get_parent().remove_child(self)
 		self.queue_free()
--- Infinity Slasher/src/scripts/World.py
+++ Infinity Slasher/src/scripts/World.py
@@ -15,24 +15,33 @@ class World(Node2D):
 	bg_velocity = platforms_velocity / 15
 	bg_acceleration = platforms_acceleration / 15
 	
+	orb_limit = 6
+	
+	player_health = 100
+	player_score = 0
+	
 	
 	def _ready(self):
 		self.background = self.get_node("Background")
 		self.platforms = self.get_node("Platforms")
 		self.orbs = self.get_node("Orbs")
 		self.orb_spawn = self.get_node("OrbSpawn")
+		self.orb_spawn_timer = self.get_node("OrbSpawnTimer")
+		self.hud = self.get_node("HUD")
 		
 		
 	def _physics_process(self, delta):
 		self.move_background(delta)
 		self.move_platforms(delta)
+		self.orb_spawn_timer.wait_time *= 0.99999
+		self.orb_limit += 0.0001
 		
 		
 	def _on_OrbSpawnTimer_timeout(self):
-		if self.orbs.get_child_count() < 6:
+		if self.orbs.get_child_count() < int(self.orb_limit):
 			orb_position = self.orb_spawn.position
 			orb_velocity = Vector2(
-				randint(-150, 0),
+				randint(-150, -50),
 				randint(-150, 150)
 			)
 			
@@ -50,6 +59,14 @@ class World(Node2D):
 		self.add_child(death_particles)
 		death_particles.global_position = orb.global_position
 		death_particles.restart()
+		
+		self.player_score += 1
+		self.hud.set_score(self.player_score)
+			
+			
+	def damage_player(self, damage):
+		self.player_health -= damage
+		self.hud.set_health(self.player_health)
 		
 		
 	def move_background(self, delta):
